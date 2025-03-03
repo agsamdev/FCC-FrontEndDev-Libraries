5848d850d1010e620e379f747da677d6948332ae
Gian Paris C. Agsam <elitefonder034@gmail.com>
2025-03-03 21:38:35 +0800

    Update App.tsx

--- Certification-Projects/markdown-reviewer/src/App.tsx
+++ Certification-Projects/markdown-reviewer/src/App.tsx
@@ -1,11 +1,9 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import ReactMarkDown from 'react-markdown'
 
 function App() {
-  const [markdownText, setMarkdownText] = useState(0)
+    const [markdownText, setMarkdownText] = useState<number>(0); 
 
   return (
     <>
